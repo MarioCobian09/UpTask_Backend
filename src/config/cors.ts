851f9816d7bbc3cc9fa08492bab4fb258b6ddf48
Mario Cobian <marvanis@hotmail.com>
2025-09-10 02:37:32 -0600

    [FIXED]: Probando errores

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -8,9 +8,12 @@ export const corsConfig: CorsOptions = {
         
         const whitelist = [process.env.FRONTEND_URL]
 
-        if(process.argv[2] === '--api') { // Con argv obtenemos todos los argumentos del script ejecutado, entonces, si tiene ese --api
+        if(process.argv.includes("--api")) { // Con argv obtenemos todos los argumentos del script ejecutado, entonces, si tiene ese --api
             whitelist.push(undefined)   // Le agregamos a la whitelist el undefined que es lo que nos regresa el "origin" cuando trabajamos con POSTMAN
         }
+
+        console.log("🌐 Origin recibido:", origin);
+        console.log("✅ Whitelist:", whitelist);
 
         if(whitelist.includes(origin)) { // Si el origen es una URL de nuetra whitelist, le permitirmos el acceso
             callback(null, true) // El null es un error, el true es si permitimos el acceso
